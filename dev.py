ddcb6cab5ff7ffdab45e4fe7062c45a1b6f1a81f
NajeebFarhan <najeebfarhan6345@gmail.com>
2026-03-03 07:17:31 +0530

    minor change

--- dev.py
+++ dev.py
@@ -17,18 +17,30 @@ def start_process():
 
 def stop_process(proc):
     if proc and proc.poll() is None:
-        print("Stopping old process...")
-        if sys.platform == "win32":
-            proc.send_signal(signal.CTRL_BREAK_EVENT)
-        else:
-            proc.send_signal(signal.SIGINT)
-        proc.wait()
+        print("Stopping process...")
+        try:
+            if sys.platform == "win32":
+                proc.send_signal(signal.CTRL_BREAK_EVENT)
+            else:
+                proc.send_signal(signal.SIGINT)
+            proc.wait(timeout=5)
+        except subprocess.TimeoutExpired:
+            print("Force killing...")
+            proc.kill()
 
 
 if __name__ == "__main__":
-    process = start_process()
-
-    for changes in watch("./src", recursive=True):
-        print("File change detected. Reloading...")
-        stop_process(process)
+    try:
         process = start_process()
+
+        for _ in watch("./src/", recursive=True):
+            print("File change detected. Reloading...")
+            stop_process(process)
+            process = start_process()
+
+    except KeyboardInterrupt:
+        print("\nShutting down dev server...")
+        stop_process(process)
+
+    finally:
+        sys.exit(0)
