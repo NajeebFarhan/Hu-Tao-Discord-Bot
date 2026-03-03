40b1a8be9b89516ed8dae4bff5118af0bd5c47ab
NajeebFarhan <najeebfarhan6345@gmail.com>
2026-03-03 06:44:08 +0530

    minor development environment optimization

--- dev.py
+++ dev.py
@@ -1,26 +1,34 @@
 import subprocess
-import os
 import signal
+import sys
 from watchfiles import watch
 
 process = None
 
-def start():
-    global process
-    process = subprocess.Popen(
+
+def start_process():
+    return subprocess.Popen(
         ["uv", "run", "src/bot.py"],
         creationflags=subprocess.CREATE_NEW_PROCESS_GROUP
+        if sys.platform == "win32"
+        else 0,
     )
 
-def stop():
-    global process
-    if process and process.poll() is None:
-        process.send_signal(signal.CTRL_BREAK_EVENT)
-        process.wait()
+
+def stop_process(proc):
+    if proc and proc.poll() is None:
+        print("Stopping old process...")
+        if sys.platform == "win32":
+            proc.send_signal(signal.CTRL_BREAK_EVENT)
+        else:
+            proc.send_signal(signal.SIGINT)
+        proc.wait()
+
 
 if __name__ == "__main__":
-    start()
-    for _ in watch("."):
-        print("Change detected. Restarting...")
-        stop()
-        start()
+    process = start_process()
+
+    for changes in watch(".", recursive=True):
+        print("File change detected. Reloading...")
+        stop_process(process)
+        process = start_process()
